2f14dc7e8f794e1746a0904e42fd12f013788cb4
Jacob Perez <rrattusss@gmail.com>
2025-04-30 00:28:43 -0700

    blah

--- Assets/Francis Stuff/Scripts/KeyManager.cs
+++ Assets/Francis Stuff/Scripts/KeyManager.cs
@@ -5,12 +5,4 @@ public class KeyManager : MonoBehaviour
 {
     // Simple script for one key and one door
    public bool hasKey = false;
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("PlayerObject")){
-            hasKey = true;
-            Destroy(gameObject);
-        }
-    }
 }
