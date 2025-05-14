3f1f53a8f98002fdf1b3498551e79d9ede054ade
rohans0 <159118768+rohans0@users.noreply.github.com>
2025-05-14 13:39:28 -0700

    built alpha playtest to web

--- Assets/RohanStuff/GuardShooter/Egg.cs
+++ Assets/RohanStuff/GuardShooter/Egg.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Egg : MonoBehaviour
 {
-	private float timeTilDisabled = 10000;
-	const float timeToDestroyAfterDisabled = .04f;
+    private float timeTilDisabled = 10000;
+    const float timeToDestroyAfterDisabled = .04f;
 
     void Update()
     {
@@ -12,7 +12,7 @@ public class Egg : MonoBehaviour
             Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad),
             0
         ) * Time.deltaTime * 5.0f;
-        
+
         newPos = Camera.main.WorldToScreenPoint(newPos + transform.position);
 
         if (newPos.x < -2 || newPos.x > Screen.width + 2 ||
@@ -23,22 +23,23 @@ public class Egg : MonoBehaviour
 
         transform.position = Camera.main.ScreenToWorldPoint(newPos);
 
-		if (timeTilDisabled < -timeToDestroyAfterDisabled) Destroy(gameObject);
-		GetComponent<BoxCollider2D>().enabled = timeTilDisabled > 0;
-		timeTilDisabled -= Time.deltaTime;
+        if (timeTilDisabled < -timeToDestroyAfterDisabled) Destroy(gameObject);
+        GetComponent<BoxCollider2D>().enabled = timeTilDisabled > 0;
+        timeTilDisabled -= Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D c)
     {
-		if (timeTilDisabled > 1 && (c.gameObject.tag == "Red Player" || c.gameObject.tag == "Blue Player"))
-		{
+        if (timeTilDisabled > 1 && (c.gameObject.tag == "Red Player" || c.gameObject.tag == "Blue Player"))
+        {
             c.gameObject.GetComponentInParent<PlayerManager>().score--;
-			timeTilDisabled = .2f;
-   //          c.gameObject.GetComponentInParent<PlayerManager>().TakeHit();
-			// timeTilDisabled = .0f;
-		}
-		else if (c.gameObject.tag != "Guard") {
-			timeTilDisabled = .2f;
-		}
+            timeTilDisabled = .2f;
+		   //          c.gameObject.GetComponentInParent<PlayerManager>().TakeHit();
+		   // timeTilDisabled = .0f;
+        }
+        else if (c.gameObject.tag != "Guard")
+        {
+            timeTilDisabled = .2f;
+        }
     }
 }
