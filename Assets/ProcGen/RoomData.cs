1fb3a644decf70f498032bd457e79950f9fca57a
Jacob Perez <rrattusss@gmail.com>
2025-05-21 12:09:27 -0700

    navmehs weirdness

--- Assets/ProcGen/RoomData.cs
+++ Assets/ProcGen/RoomData.cs
@@ -8,6 +8,20 @@ public class RoomData : MonoBehaviour
     [SerializeField] public int2 exitPos;
     [SerializeField] public DoorSide entranceSide, exitSide;
 
+    public void ToggleGuards(bool on)
+    {
+        ToggleGuardsR(transform, on);
+    }
+
+    public void ToggleGuardsR(Transform t, bool on)
+    {
+        for (int i = 0; i < t.childCount; i++)
+        {
+            if (t.GetChild(i).GetComponent<LavaGuard>() != null) t.GetChild(i).gameObject.SetActive(on);
+            ToggleGuardsR(t.GetChild(i), on);
+        }
+    }
+
     public enum DoorSide
     {
         Top,
