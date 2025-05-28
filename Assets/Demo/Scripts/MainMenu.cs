1a81b55fadadd3ea6a711a60c3ff8039bb6979b8
Francis Ricafrente <58997963+Draftier@users.noreply.github.com>
2025-05-27 23:31:51 -0700

    - Implemented basic menu UI
    - TODO implement settings menu to mess around with audio
    - Also figured out that pause menu not working in procedural because event system not generated in new scene

--- Assets/Demo/Scripts/MainMenu.cs
+++ Assets/Demo/Scripts/MainMenu.cs
@@ -3,9 +3,63 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+	public GameObject mainMenuPanel;
+	public GameObject levelMenuPanel;
+	// public GameObject settingsMenuPanel; Maybe add later (need to utilize data persistence between scenes and mess around with how audio managed)
+	public GameObject playPanel;
+
+	private void Awake()
+	{
+		// Doesn't find inactive objects, so we need to assign them in the inspector
+		// mainMenuPanel = GameObject.Find("MainMenuPanel");
+		// levelMenuPanel = GameObject.Find("LevelMenuPanel");
+		// settingsMenuPanel = GameObject.Find("SettingsMenuPanel");
+		// playPanel = GameObject.Find("PlayPanel");
+	}
+
+    private void Start()
+    {
+    	// Ensure only the main menu is active at the start
+		mainMenuPanel.SetActive(true);
+		levelMenuPanel.SetActive(false);
+		// settingsMenuPanel.SetActive(false);
+		playPanel.SetActive(false);   
+    }
     public void LoadScene(int sceneBuildIndex)
 	{
 		// find in File > Build Profiles
 		SceneManager.LoadScene(sceneBuildIndex);
+	}
+
+	public void LoadLevelMenu()
+	{
+		mainMenuPanel.SetActive(false);
+		// settingsMenuPanel.SetActive(false);
+		playPanel.SetActive(false);
+		levelMenuPanel.SetActive(true);
+	}
+
+	public void LoadSettingsMenu()
+	{
+		mainMenuPanel.SetActive(false);
+		levelMenuPanel.SetActive(false);
+		playPanel.SetActive(false);
+		// settingsMenuPanel.SetActive(true);
+	}
+
+	public void BackToMainMenu()
+	{
+		levelMenuPanel.SetActive(false);
+		// settingsMenuPanel.SetActive(false);
+		playPanel.SetActive(false);
+		mainMenuPanel.SetActive(true);
+	}
+
+	public void LoadPlayPanel()
+	{
+		mainMenuPanel.SetActive(false);
+		levelMenuPanel.SetActive(false);
+		// settingsMenuPanel.SetActive(false);
+		playPanel.SetActive(true);
 	}
 }
